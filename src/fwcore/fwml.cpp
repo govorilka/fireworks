3ae6f85ecc296d3d6f6123abe0d5a92368abdbd7
Egor Popov <Ohjelmoijana@gmail.com>
2010-12-22 00:49:02 +0600

    fwcore::ML: Added first parse command to FwMLObject.

--- src/fwcore/fwml.cpp
+++ src/fwcore/fwml.cpp
@@ -6,38 +6,102 @@
 
 namespace
 {
-    enum CharsType
+    enum CharType
     {
-        C_Unknow = 0, //Unknow
-        C_Alpha,      //Alpha (A..Z, a..z)
-        C_QuMark,     //Quotation mark (")
-
+        C_AZ,      //Alpha (A..Z, a..z)
+        C_Num,     //Numbers (0..9)
+        C_Sp,      //Space (' ')
+        C_Str,     //Quotation mark (")
+        C_Col,     //Name separator, colon (:)
+        C_LCu,     //Begin-object, left curly bracket ({)
+        C_RCu,     //End-object, right curly bracket (})
+        C_Poi,     //Decimal point (.)
+        C_Uni,     //Unicode symbol
+        C_Err,     //Unknow
+        C_MAX = C_Err
     };
 
     const char chars_type[128] = {
 
-          /* 0  */  /* 1  */  /* 2  */  /* 3  */  /* 4  */  /* 5  */  /* 6  */  /* 7  */
-/*  0 */  C_Unknow, C_Unknow, C_Unknow, C_Unknow, C_Unknow, C_Unknow, C_Unknow, C_Unknow,
-/*  8 */  C_Unknow, C_Unknow, C_Unknow, C_Unknow, C_Unknow, C_Unknow, C_Unknow, C_Unknow,
-/* 16 */  C_Unknow, C_Unknow, C_Unknow, C_Unknow, C_Unknow, C_Unknow, C_Unknow, C_Unknow,
-/* 24 */  C_Unknow, C_Unknow, C_Unknow, C_Unknow, C_Unknow, C_Unknow, C_Unknow, C_Unknow,
+          /*0 */ /*1 */ /*2 */ /*3 */ /*4 */ /*5 */ /*6 */ /*7 */
+/*  0 */  C_Err, C_Err, C_Err, C_Err, C_Err, C_Err, C_Err, C_Err,
+/*  8 */  C_Err, C_Sp,  C_Err, C_Err, C_Err, C_Err, C_Err, C_Err,
+/* 16 */  C_Err, C_Err, C_Err, C_Err, C_Err, C_Err, C_Err, C_Err,
+/* 24 */  C_Err, C_Err, C_Err, C_Err, C_Err, C_Err, C_Err, C_Err,
 
-/* 32 */  C_Unknow, C_Unknow, C_QuMark, C_Unknow, C_Unknow, C_Unknow, C_Unknow, C_Unknow,
-/* 40 */  C_Unknow, C_Unknow, C_Unknow, C_Unknow, C_Unknow, C_Unknow, C_Unknow, C_Unknow,
-/* 48 */  C_Unknow, C_Unknow, C_Unknow, C_Unknow, C_Unknow, C_Unknow, C_Unknow, C_Unknow,
-/* 56 */  C_Unknow, C_Unknow, C_Unknow, C_Unknow, C_Unknow, C_Unknow, C_Unknow, C_Unknow,
+/* 32 */  C_Sp,  C_Err, C_Str, C_Err, C_Err, C_Err, C_Err, C_Err,
+/* 40 */  C_Err, C_Err, C_Err, C_Err, C_Err, C_Err, C_Poi,  C_Err,
+/* 48 */  C_Num, C_Num, C_Num, C_Num, C_Num, C_Num, C_Num, C_Num,
+/* 56 */  C_Num, C_Num, C_Col, C_Err, C_Err, C_Err, C_Err, C_Err,
 
-/* 64 */  C_Unknow, C_Alpha,  C_Alpha,  C_Alpha,  C_Alpha,  C_Alpha,  C_Alpha,  C_Alpha,
-/* 72 */  C_Alpha,  C_Alpha,  C_Alpha,  C_Alpha,  C_Alpha,  C_Alpha,  C_Alpha,  C_Alpha,
-/* 80 */  C_Alpha,  C_Alpha,  C_Alpha,  C_Alpha,  C_Alpha,  C_Alpha,  C_Alpha,  C_Alpha,
-/* 88 */  C_Alpha,  C_Alpha,  C_Alpha,  C_Unknow, C_Unknow, C_Unknow, C_Unknow, C_Unknow,
+/* 64 */  C_Err, C_AZ,  C_AZ,  C_AZ,  C_AZ,  C_AZ,  C_AZ,  C_AZ,
+/* 72 */  C_AZ,  C_AZ,  C_AZ,  C_AZ,  C_AZ,  C_AZ,  C_AZ,  C_AZ,
+/* 80 */  C_AZ,  C_AZ,  C_AZ,  C_AZ,  C_AZ,  C_AZ,  C_AZ,  C_AZ,
+/* 88 */  C_AZ,  C_AZ,  C_AZ,  C_Err, C_Err, C_Err, C_Err, C_Err,
 
-/* 96 */  C_Unknow, C_Alpha,  C_Alpha,  C_Alpha,  C_Alpha,  C_Alpha,  C_Alpha,  C_Alpha,
-/* 104*/  C_Alpha,  C_Alpha,  C_Alpha,  C_Alpha,  C_Alpha,  C_Alpha,  C_Alpha,  C_Alpha,
-/* 112*/  C_Alpha,  C_Alpha,  C_Alpha,  C_Alpha,  C_Alpha,  C_Alpha,  C_Alpha,  C_Alpha,
-/* 120*/  C_Alpha,  C_Alpha,  C_Alpha,  C_Unknow, C_Unknow, C_Unknow, C_Unknow, C_Unknow,
+/* 96 */  C_Err, C_AZ,  C_AZ,  C_AZ,  C_AZ,  C_AZ,  C_AZ,  C_AZ,
+/* 104*/  C_AZ,  C_AZ,  C_AZ,  C_AZ,  C_AZ,  C_AZ,  C_AZ,  C_AZ,
+/* 112*/  C_AZ,  C_AZ,  C_AZ,  C_AZ,  C_AZ,  C_AZ,  C_AZ,  C_AZ,
+/* 120*/  C_AZ,  C_AZ,  C_AZ,  C_LCu, C_Err, C_RCu, C_Err, C_Err,
 
     };
+
+    struct ParseData;
+    typedef void(*CommandFunc)(char c, ParseData* data);
+
+    void x_var(char c, ParseData* data);
+    void x_nva(char c, ParseData* data);
+    void x_val(ParseData* data);
+    void x_str(ParseData* data);
+    void x_bst(ParseData* data);
+    void x_est(ParseData* data);
+    void x_num(ParseData* data);
+    void x_rnu(ParseData* data);
+    void x_err(ParseData* data);
+    void x_obj(ParseData* data);
+    void x_eob(ParseData* data);
+
+    enum
+    {
+        X_VAR = 0,
+        X_MAX = 2
+    };
+
+    const CommandFunc parse_commands[X_MAX][C_MAX] = {
+/*            C_AZ,  C_Num,   C_Sp,  C_Str,  C_Col,  C_LCu,  C_RCu,  C_Poi, C_Uni     */
+/*X_VAR*/{  &x_var, &x_nva,      0,      0,      0,      0,      0,      0,     0  },
+    };
+
+    struct ParseData
+    {
+        ParseData();
+
+        bool specialChar;
+        QByteArray string;
+        int xcmd;
+    };
+
+    ParseData::ParseData() :
+        specialChar(false),
+        xcmd(X_VAR)
+    {
+    }
+
+    void x_var(char c, ParseData* data)
+    {
+        data->xcmd = X_VAR;
+        data->string += c;
+    }
+
+    void x_nva(char c, ParseData* data)
+    {
+        if(data->string.isEmpty())
+        {
+            return; //TODO:error!!!
+        }
+        data->xcmd = X_VAR;
+        data->string += c;
+    }
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -203,18 +267,22 @@ bool FwMLObject::parse(QIODevice* ioDevice)
 {
     if(ioDevice->open(QIODevice::ReadOnly | QIODevice::Text))
     {
+        ParseData data;
+        CommandFunc cmd = 0;
         while(!ioDevice->atEnd())
         {
             QByteArray line = ioDevice->readLine().trimmed();
             if(!line.isEmpty())
             {
-                qDebug() << line;
                 foreach(char c, line)
                 {
-                    if(c < 128)
+                    char type = c > 0 ? chars_type[c] : C_Uni;
+                    if(type != C_Err)
                     {
-                        char type = chars_type[c];
-                        qDebug() << c << static_cast<int>(type);
+                        if(cmd = parse_commands[data.xcmd][type])
+                        {
+                            cmd(c, &data);
+                        }
                     }
                 }
             }
