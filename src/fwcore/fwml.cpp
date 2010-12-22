310f093173c882d74c026fe25ffd70d5c108b508
Egor Popov <Ohjelmoijana@gmail.com>
2010-12-23 00:37:27 +0600

    fwcore::FwML: Added error handling to FwML parser.

--- src/fwcore/fwml.cpp
+++ src/fwcore/fwml.cpp
@@ -4,8 +4,24 @@
 
 #include "fwml.h"
 
+//Parse utils
 namespace
 {
+    struct ParseData;
+
+    class FwMLParserException : public std::exception
+    {
+        typedef std::exception BaseClass;
+    public:
+        FwMLParserException(const QString& err);
+        FwMLParserException(char c, ParseData* data);
+        ~FwMLParserException() throw();
+
+        const char* what() const throw();
+
+        QString message;
+    };
+
     enum CharType
     {
         C_AZ,      //Alpha (A..Z, a..z)
@@ -21,6 +37,7 @@ namespace
         C_MAX = C_Err
     };
 
+    //Types of ASCII chars
     const char chars_type[128] = {
 
           /*0 */ /*1 */ /*2 */ /*3 */ /*4 */ /*5 */ /*6 */ /*7 */
@@ -46,11 +63,11 @@ namespace
 
     };
 
-    struct ParseData;
     typedef void(*CommandFunc)(char c, ParseData* data);
 
-    void x_var(char c, ParseData* data);
-    void x_nva(char c, ParseData* data);
+    void x_var(char c, ParseData* data) throw(FwMLParserException&);
+    void x_nva(char c, ParseData* data) throw(FwMLParserException&);
+    void x_bob(char c, ParseData* data) throw(FwMLParserException&);
     void x_val(ParseData* data);
     void x_str(ParseData* data);
     void x_bst(ParseData* data);
@@ -67,40 +84,98 @@ namespace
         X_MAX = 2
     };
 
+    //Parse command or parse state
     const CommandFunc parse_commands[X_MAX][C_MAX] = {
 /*            C_AZ,  C_Num,   C_Sp,  C_Str,  C_Col,  C_LCu,  C_RCu,  C_Poi, C_Uni     */
-/*X_VAR*/{  &x_var, &x_nva,      0,      0,      0,      0,      0,      0,     0  },
+/*X_VAR*/{  &x_var, &x_nva,      0,      0,      0, &x_bob,      0,      0,     0  },
     };
 
     struct ParseData
     {
         ParseData();
 
+        QByteArray attribute;
+        FwMLObject* object;
+
         bool specialChar;
         QByteArray string;
         int xcmd;
+
+        int line;
+        int column;
     };
 
     ParseData::ParseData() :
         specialChar(false),
-        xcmd(X_VAR)
+        xcmd(X_VAR),
+        line(0),
+        column(0),
+        object(0)
     {
     }
 
-    void x_var(char c, ParseData* data)
+
+    FwMLParserException::FwMLParserException(char c, ParseData* data) :
+        BaseClass()
+    {
+        message = QString("Unexcepted char '%1', line %2, column %3")
+                  .arg(c)
+                  .arg(data->line)
+                  .arg(data->column);
+    }
+
+    FwMLParserException::FwMLParserException(const QString& err) :
+        BaseClass(),
+        message(err)
+    {
+    }
+
+    const char* FwMLParserException::what() const throw()
+    {
+        return qPrintable(message);
+    }
+
+    FwMLParserException::~FwMLParserException() throw()
+    {
+    }
+
+    void x_var(char c, ParseData* data) throw(FwMLParserException&)
     {
         data->xcmd = X_VAR;
         data->string += c;
     }
 
-    void x_nva(char c, ParseData* data)
+    void x_nva(char c, ParseData* data) throw(FwMLParserException&)
     {
         if(data->string.isEmpty())
         {
-            return; //TODO:error!!!
+            throw FwMLParserException(c, data);
+            return;
         }
         data->xcmd = X_VAR;
         data->string += c;
+    }
+
+    void x_bob(char c, ParseData* data) throw(FwMLParserException&)
+    {
+        if(!data->string.isEmpty())
+        {
+            data->attribute = data->string;
+            data->string = "";
+        }
+
+        if(data->object && !data->attribute.isEmpty())
+        {
+            FwMLObject* newObject = new FwMLObject();
+
+            data->xcmd = X_VAR;
+            data->object->addAttribute(data->attribute, newObject);
+            data->object = newObject;
+            data->attribute = "";
+            return;
+        }
+
+        throw FwMLParserException(c, data);
     }
 }
 
@@ -265,17 +340,27 @@ bool FwMLObject::parse(const QByteArray& utf8String)
 */
 bool FwMLObject::parse(QIODevice* ioDevice)
 {
-    if(ioDevice->open(QIODevice::ReadOnly | QIODevice::Text))
+    try
     {
+        if(!ioDevice->open(QIODevice::ReadOnly | QIODevice::Text))
+        {
+            throw FwMLParserException(ioDevice->errorString());
+        }
+
         ParseData data;
         CommandFunc cmd = 0;
         while(!ioDevice->atEnd())
         {
+            data.line++;
+            data.column = 0;
             QByteArray line = ioDevice->readLine().trimmed();
             if(!line.isEmpty())
             {
-                foreach(char c, line)
+                int line_size = line.size();
+                char* c_ptr = line.data();
+                for(;data.column < line_size; data.column++, c_ptr++)
                 {
+                    char& c = (*c_ptr);
                     char type = c > 0 ? chars_type[c] : C_Uni;
                     if(type != C_Err)
                     {
@@ -287,9 +372,14 @@ bool FwMLObject::parse(QIODevice* ioDevice)
                 }
             }
         }
-        return true;
     }
-    return false;
+    catch(FwMLParserException& e)
+    {
+        qDebug() << e.what();
+        return false;
+    }
+
+    return true;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
