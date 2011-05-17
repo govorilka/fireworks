d1bfaca8a3d74389bea1707d74d961ae37e55c50
Egor Popov <e.popov@sibset-team.ru>
2011-05-17 19:48:48 +0700

    Added new class FwNetworkConfig

---
+++ src/fwutils/fwnetworkconfig.h
@@ -0,0 +1,44 @@
+#ifndef FIREWORKS_NETWORKCONFIG_H
+#define FIREWORKS_NETWORKCONFIG_H
+
+#include <QtCore/qobject.h>
+
+#include <QtNetwork/qnetworkinterface.h>
+
+class FwNetworkConfig : public QObject
+{
+    Q_OBJECT
+    typedef QObject BaseClass;
+
+public:
+    explicit FwNetworkConfig(QObject *parent = 0);
+
+    inline QString activeInterfaceName() const;
+    inline void setActiveInterfaceName(const QString& name);
+
+    inline QNetworkInterface activeInterface() const;
+
+private:
+    QString m_activeInterface;
+};
+
+QString FwNetworkConfig::activeInterfaceName() const
+{
+    return m_activeInterface;
+}
+
+void FwNetworkConfig::setActiveInterfaceName(const QString& name)
+{
+    m_activeInterface = name;
+}
+
+QNetworkInterface FwNetworkConfig::activeInterface() const
+{
+    if(!m_activeInterface.isEmpty())
+    {
+        return QNetworkInterface::interfaceFromName(m_activeInterface);
+    }
+    return QNetworkInterface();
+}
+
+#endif // FIREWORKS_NETWORKCONFIG_H
