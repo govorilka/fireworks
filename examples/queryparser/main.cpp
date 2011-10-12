2dd534c758f9ef0c39786bdb627d10737e4d7915
Sergey Kiushkin <Sergey Kiushkin@localhost>
2011-10-12 12:35:09 +0700

    FwPg added realiration doColumnInt and doColumnBool and tested thiw server

--- examples/queryparser/main.cpp
+++ examples/queryparser/main.cpp
@@ -22,6 +22,7 @@ int main(int argc, char **argv)
 
     qDebug() << endl << "#Parse end" << endl;
 
+    /////////////////////////////////////////////////////////////////////
     QString connection;
     if(argc > 1)
     {
@@ -38,17 +39,35 @@ int main(int argc, char **argv)
         db.open(connection);
         db.beginTransaction();
 
-        Fw::Query query1 = db.query("DECLARE myportal CURSOR FOR select * from pg_database");
-        query1.step();
+//        Fw::Query query1 = db.query("DECLARE myportal CURSOR FOR select * from pg_database");
+//        query1.step();
 
-        Fw::Query query2 = db.query("FETCH ALL in myportal");
+//        Fw::Query query2 = db.query("FETCH ALL in myportal");
+//        while(query2.step())
+//        {
+//            printf("%-15s", query2.columnText(0).toUtf8().constData());
+//        }
+
+//        Fw::Query query3 = db.query("CLOSE myportal");
+//        query3.step();
+
+        Fw::Query query2 = db.query("SELECT contoencoding FROM pg_conversion");
+        qDebug() << "contoencoding";
         while(query2.step())
         {
-            printf("%-15s", query2.columnText(0).toUtf8().constData());
+            qDebug() << query2.columnInt(0);
         }
 
-        Fw::Query query3 = db.query("CLOSE myportal");
-        query3.step();
+        Fw::Query query3 = db.query("SELECT datallowconn FROM pg_database");
+        qDebug() << "\n////////////////////////////////////////////\n";
+        qDebug() << "datallowcon";
+        while(query3.step())
+        {
+            if(query3.columnBool(0))
+                qDebug() << "_true";
+            else
+                qDebug() << "_false";
+        }
 
         db.commit();
         db.close();
