9e2b39ca290e727ac9df670b5067516723696cb3
Egor Popov <e.popov@sibset-team.ru>
2011-11-25 18:23:08 +0700

    Added new classes Fw::ML::UIntNumber

--- src/fw/ml/ml.pri
+++ src/fw/ml/ml.pri
@@ -3,13 +3,19 @@ HEADERS += \
     fw/ml/node.hpp \
     fw/ml/object.hpp \
     fw/ml/string.hpp \
-    fw/ml/array.hpp
+    fw/ml/array.hpp \
+    fw/ml/uintnumber.hpp
 
 SOURCES += \
     fw/ml/node.cpp \
     fw/ml/string.cpp \
     fw/ml/array.cpp \
-    fw/ml/object.cpp
+    fw/ml/object.cpp \
+    fw/ml/uintnumber.cpp
+
+
+
+
 
 
 
---
+++ src/fw/ml/uintnumber.hpp
@@ -0,0 +1,30 @@
+#ifndef FIREWORKS_ML_UINTNUMBER_H
+#define FIREWORKS_ML_UINTNUMBER_H
+
+#include "fw/ml/node.hpp"
+
+class FIREWORKSSHARED_EXPORT Fw::ML::UIntNumber : public Fw::ML::BaseTempl<Fw::ML::T_UIntNumber>
+{
+    typedef Fw::ML::BaseTempl<Fw::ML::T_UIntNumber> BaseClass;
+
+    quint32 m_value;
+
+public:
+    UIntNumber();
+    UIntNumber(quint32 value);
+    UIntNumber(quint32 value, const QByteArray& attrName, Object* parent);
+    UIntNumber(quint32 value, Array* parent);
+
+    inline quint32 value() const;
+    inline void setValue(quint32 value);
+
+    QByteArray toUtf8() const;
+
+    int toInt(bool* bOk) const;
+    bool toBool(bool* bOk) const;
+    FwColor toColor(bool* bOk) const;
+
+    Node* clone() const;
+};
+
+#endif //FIREWORKS_ML_UINTNUMBER_H
