d3c79eae1b1c9f958628d1c4422bfd27cb059e54
Egor Popov <e.popov@sibset-team.ru>
2011-12-12 12:41:24 -0700

    Small changed in Arguments class

--- src/fw/console/arguments.hpp
+++ src/fw/console/arguments.hpp
@@ -33,7 +33,7 @@ public:
     ~Arguments();
 
     QString value(const QString& key, const QString& defaultValue = QString()) const;
-    bool isExist(const QString& key, QString& value) const;
+    bool isExist(const QString& key, QString* value = 0) const;
     inline const ArgumentList arguments() const;
 
 private:
