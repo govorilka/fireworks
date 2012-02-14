0b74ee3759275c240de235121305cb3166c79efa
Egor Popov <e.popov@sibset-team.ru>
2012-02-14 14:59:45 +0700

    Refactoring JSON API

---
+++ src/fw/core/json_inl.hpp
@@ -0,0 +1,163 @@
+#ifndef FIREWORKS_CORE_JSON_INL_HPP
+#define FIREWORKS_CORE_JSON_INL_HPP
+
+#include "fw/core/json.hpp"
+
+Fw::JSON::Node* Fw::JSON::Node::parent() const
+{
+    return m_parent;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+QString Fw::JSON::String::value() const
+{
+    return m_value;
+}
+
+void Fw::JSON::String::setValue(const QString& value)
+{
+    m_value = value;
+}
+
+bool Fw::JSON::String::isEmpty() const
+{
+    return m_value.isEmpty();
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+double Fw::JSON::Number::value() const
+{
+    return m_value;
+}
+
+void Fw::JSON::Number::setValue(double value)
+{
+    m_value = value;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+bool Fw::JSON::Boolean::value() const
+{
+    return m_value;
+}
+
+void Fw::JSON::Boolean::setValue(bool value)
+{
+    m_value = value;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+Fw::JSON::Node* Fw::JSON::Object::attribute(const QByteArray& name) const
+{
+    return m_attributes.value(name, 0);
+}
+
+QByteArray Fw::JSON::Object::attributeName(Fw::JSON::Node* child) const
+{
+    return m_attributes.key(child, "");
+}
+
+QHash<QByteArray, Fw::JSON::Node*> Fw::JSON::Object::attributes() const
+{
+    return m_attributes;
+}
+
+QList<Fw::JSON::Node*> Fw::JSON::Object::toList() const
+{
+    return m_attributes.values();
+}
+
+void Fw::JSON::Object::removeAttribute(const QByteArray& name)
+{
+    if(m_attributes.contains(name))
+    {
+        delete m_attributes.take(name);
+    }
+}
+
+int Fw::JSON::Object::attributesCount() const
+{
+    return m_attributes.size();
+}
+
+Fw::JSON::String* Fw::JSON::Object::addString(const QByteArray& name, const QString& value)
+{
+    return static_cast<String*>(addAttribute(name, new String(value)));
+}
+
+Fw::JSON::Number* Fw::JSON::Object::addNumber(const QByteArray& name, double value)
+{
+    return static_cast<Number*>(addAttribute(name, new Number(value)));
+}
+
+Fw::JSON::Boolean* Fw::JSON::Object::addBoolean(const QByteArray& name, bool value)
+{
+    return static_cast<Boolean*>(addAttribute(name, new Boolean(value)));
+}
+
+Fw::JSON::Object* Fw::JSON::Object::addObject(const QByteArray& name)
+{
+    return static_cast<Object*>(addAttribute(name, new Object()));
+}
+
+Fw::JSON::Array* Fw::JSON::Object::addArray(const QByteArray& name)
+{
+    return static_cast<Array*>(addAttribute(name, new Array()));
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+int Fw::JSON::Array::size() const
+{
+    return m_data.size();
+}
+
+int Fw::JSON::Array::indexOf(Fw::JSON::Node* item) const
+{
+    return m_data.indexOf(item);
+}
+
+Fw::JSON::Node* Fw::JSON::Array::item(int index) const
+{
+    if(index < m_data.size() && index >= 0)
+    {
+        return m_data.at(index);
+    }
+    return 0;
+}
+
+QVector<Fw::JSON::Node*> Fw::JSON::Array::toQVector() const
+{
+    return m_data;
+}
+
+Fw::JSON::String* Fw::JSON::Array::addString(const QString& value)
+{
+    return static_cast<String*>(addValue(new String(value)));
+}
+
+Fw::JSON::Number* Fw::JSON::Array::addNumber(double value)
+{
+    return static_cast<Number*>(addValue(new Number(value)));
+}
+
+Fw::JSON::Boolean* Fw::JSON::Array::addBoolean(bool value)
+{
+    return static_cast<Boolean*>(addValue(new Boolean(value)));
+}
+
+Fw::JSON::Object* Fw::JSON::Array::addObject()
+{
+    return static_cast<Object*>(addValue(new Object()));
+}
+
+Fw::JSON::Array* Fw::JSON::Array::addArray()
+{
+    return static_cast<Array*>(addValue(new Array()));
+}
+
+#endif // FIREWORKS_CORE_JSON_INL_HPP
